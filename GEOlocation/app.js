77a19660b3709edf1bbe31ed182d66d2ec3808f5
student <student@student-RnD>
2017-07-11 11:04:25 +0300

    asdf

--- GEOlocation/app.js
+++ GEOlocation/app.js
@@ -47,7 +47,21 @@ function getCoord() {
 
 function success(pos) {
 	  var crd = pos.coords;
-		  console.log(crd.latitude+" "+crd.longitude);
+//		  console.log(crd.latitude+" "+crd.longitude);
+			$.ajax({
+			    url: 'http://83.69.213.178:8081/check_point?game_id=10&latitude='+crd.latitude+'&longitude='+crd.longitude,             // указываем URL и
+			    dataType : "json",                     // тип загружаемых данных
+			    success: function (data, textStatus) { // вешаем свой обработчик на функцию success
+			    console.log("status:"+data.game_id+" distance:"+ data.distance+" remainded_points:"+data.remainded_points+" current_point:"+data.current_point);
+			    console.log("status of quory:"+textStatus);
+			    document.body.innerHTML= ' <div class="ui-content content-padding"><p >status '+data.game_id+'</p><p>distance '+data.distance+'</p><p>remainded_points '+data.remainded_points+'</p><p>current_point '+data.current_point+'</p> <button id="getPost" onclick="getCoord()">getINF </button></div>';
+				
+				
+				
+				
+
+			    }
+			})
 
 
 	};
@@ -57,13 +71,5 @@ function success(pos) {
 		
 function getLoc()
 {		
-	$.ajax({
-	    url: 'http://83.69.213.178:8081/check_point?game_id=10&latitude=55.796376&longitude=37.619942',             // указываем URL и
-	    dataType : "json",                     // тип загружаемых данных
-	    success: function (data, textStatus) { // вешаем свой обработчик на функцию success
-	    console.log("status:"+data.game_id+" distance:"+ data.distance+" remainded_points:"+data.remainded_points+" current_point:"+data.current_point);
-	    console.log("status:"+textStatus);
-	        
-	    }
-	})
+
 }
